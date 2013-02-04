196668f75850500b2e7226de9204d52f74445eef
Ross Phillips <really.fake@really.really.really.fake>
2013-02-04 22:51:44 +1300

    Basic funtionality

---
+++ scripts/scout.js
@@ -0,0 +1,31 @@
+angular.module('scout', ['ngResource'])
+
+.factory('GooglePlus', ['$resource', function($resource) {
+	return $resource('https://www.googleapis.com/plus/v1/activities', {}, {get: {method: 'JSONP', params: {key: 'AIzaSyDfmmrGRX43dlB9jMiCjyN2Zv1toEq-ZXk', callback: 'JSON_CALLBACK'}}});
+}])
+
+.factory('Twitter', ['$resource', function($resource) {
+	return $resource('http://search.twitter.com/search.json', {}, {get: {method: 'JSONP', params: {callback: 'JSON_CALLBACK'}}});
+}])
+
+.factory('Facebook', ['$resource', function($resource) {
+	return $resource('https://graph.facebook.com/search', {}, {get: {method: 'JSONP', params: {q: '@q', type: 'post', callback: 'JSON_CALLBACK'}}});
+}]);
+
+function ScoutCtrl($scope, Facebook, Twitter, GooglePlus) {
+
+	$scope.search = function() { if ($scope.query !== '') {
+		Twitter.get({q: $scope.query}, function onSuccess(result) {
+			$scope.twitterItems = result.results;
+		});
+
+		Facebook.get({q: $scope.query}, function onSuccess(result) {
+			$scope.facebookItems = result.data;
+		});
+
+		GooglePlus.get({query: $scope.query}, function onSuccess(result) {
+			$scope.googlePlus = result.items;
+		});
+	}}
+
+}
