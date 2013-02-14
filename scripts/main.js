f9ae6abfd9ba0b61052bb13e1820b53f6cb8919e
Ross Phillips <really.fake@really.really.really.fake>
2013-02-15 00:24:42 +1300

    bit of organising

--- scripts/main.js
+++ scripts/main.js
@@ -16,77 +16,111 @@ function ScoutCtrl($scope, Facebook, Twitter, GooglePlus) {
 
 	var currentQuery = '';
 
-	$scope.googlePlus = {searching: false};
-	$scope.twitter = {searching: false};
-	$scope.facebook = {searching: false};
+	$scope.googlePlus = {
+		items: [],
+		searching: false,
+		clear: function() {
+			$scope.googlePlus.items = [];
+			$scope.googlePlus.searching = false;
+			$scope.googlePlus.nextPageToken = undefined;
+		},
+		feed: function () {
+			if (!$scope.googlePlus.searching && currentQuery != '') {
+				$scope.googlePlus.searching = true;
+
+				var request = {query: currentQuery}
+				if ($scope.googlePlus.nextToken) request.pageToken = $scope.googlePlus.nextToken;
+
+				GooglePlus.get(request, function onSuccess(result) {
+					$scope.googlePlus.nextToken = result.nextPageToken;
+					$scope.googlePlus.items = $scope.googlePlus.items.concat(result.items);
+
+					window.setTimeout(function() { $scope.googlePlus.searching = false; }, 1000);
+				});
+			}
+		}
+	};
+
+	$scope.twitter = {
+		items: [],
+		searching: false,
+		clear: function() {
+			$scope.twitter.items = [];
+			$scope.twitter.searching = false;
+			$scope.twitter.maxId = undefined;
+			$scope.twitter.page = undefined;
+		},
+		feed: function() {
+			if (!$scope.twitter.searching && currentQuery != '') {
+				$scope.twitter.searching = true;
+
+				var request = {q: currentQuery};
+				if ($scope.twitter.page) request.page = $scope.twitter.page + 1;
+				if ($scope.twitter.maxId) request.max_id = $scope.twitter.maxId;
+
+				Twitter.get(request, function onSuccess(result) {
+					$scope.twitter.page = result.page;
+					$scope.twitter.maxId = result.max_id;
+					$scope.twitter.items = $scope.twitter.items.concat(result.results);
+
+					window.setTimeout(function() { $scope.twitter.searching = false; }, 1000);
+				});
+			}
+		}
+	};
+
+	$scope.facebook = {
+		items: [],
+		searching: false,
+		clear: function() {
+			$scope.facebook.items = [];
+			$scope.facebook.searching = false;
+			$scope.facebook.until = undefined;
+		},
+		feed: function() {
+			if (!$scope.facebook.searching && currentQuery != '') {
+				$scope.facebook.searching = true;
+
+				var request = {q: currentQuery}
+				if ($scope.facebook.until) request.until = $scope.facebook.until;
+
+				Facebook.get(request, function onSuccess(result) {
+					$scope.facebook.until = result.paging ? result.paging.next.match(/&until=(\d+)/)[1] : undefined;
+					$scope.facebook.items = $scope.facebook.items.concat(result.data);
+
+					window.setTimeout(function() { $scope.facebook.searching = false; }, 1000);
+				});
+			}
+		}
+	};
+
+	$scope.search = function() {
+		if ($scope.query !== '') {
+			currentQuery = $scope.query;
+
+			$scope.twitter.clear();
+			$scope.googlePlus.clear();
+			$scope.facebook.clear();
+
+			$scope.twitter.feed();
+			$scope.googlePlus.feed();
+			$scope.facebook.feed();
+		}
+	};
 
 	$("#google-plus-end").appear();
 	$("#twitter-end").appear();
 	$("#facebook-end").appear();
-
-	$scope.search = function() { if ($scope.query !== '') {
-		currentQuery = $scope.query;
-
-		Twitter.get({q: currentQuery}, function onSuccess(result) {
-			$scope.twitter.items = result.results;
-			$scope.twitter.page = result.page;
-			$scope.twitter.maxId = result.max_id_str;
-		});
-
-		Facebook.get({q: currentQuery}, function onSuccess(result) {
-			$scope.facebook.items = result.data;
-			$scope.facebook.until = result.paging.next.match(/&until=(\d+)/)[1];
-		});
-
-		GooglePlus.get({query: currentQuery}, function onSuccess(result) {
-			$scope.googlePlus.items = result.items;
-			$scope.googlePlus.nextToken = result.nextPageToken;
-		});
-	}};
+	$(document.body).on('appear', '#google-plus-end', $scope.googlePlus.feed);
+	$(document.body).on('appear', '#twitter-end', $scope.twitter.feed);
+	$(document.body).on('appear', '#facebook-end', $scope.facebook.feed);
 
 	$scope.getGooglePlusActorImage = function(url) {
 		return url.replace(/\?sz=50/i, '?sz=20');
 	};
 
-	$(document.body).on('appear', '#google-plus-end', function(e, $affected) {
-		if (!$scope.googlePlus.searching) {
-			$scope.googlePlus.searching = true;
+	$scope.has = function(key, object) {
+		return typeof object[key] != 'undefined';
+	};
 
-			var request = {query: currentQuery, pageToken: $scope.googlePlus.nextToken}
-
-			GooglePlus.get(request, function onSuccess(result) {
-				$scope.googlePlus.nextToken = result.nextPageToken;
-				$scope.googlePlus.items = $scope.googlePlus.items.concat(result.items);
-
-				window.setTimeout(function() { $scope.googlePlus.searching = false; }, 1000);
-			});
-		}
-	});
-
-	$(document.body).on('appear', '#twitter-end', function(e, $affected) {
-		if (!$scope.twitter.searching) {
-			$scope.twitter.searching = true;
-
-			Twitter.get({q: currentQuery, page: $scope.twitter.page + 1, max_id: $scope.twitter.maxId}, function onSuccess(result) {
-				$scope.twitter.page = result.page;
-				$scope.twitter.maxId = result.max_id;
-				$scope.twitter.items = $scope.twitter.items.concat(result.results);
-
-				window.setTimeout(function() { $scope.twitter.searching = false; }, 1000);
-			});
-		}
-	});
-
-	$(document.body).on('appear', '#facebook-end', function(e, $affected) {
-		if (!$scope.facebook.searching) {
-			$scope.facebook.searching = true;
-
-			Facebook.get({q: currentQuery, until: $scope.facebook.until}, function onSuccess(result) {
-				$scope.facebook.until = result.paging.next.match(/&until=(\d+)/)[1];
-				$scope.facebook.items = $scope.facebook.items.concat(result.data);
-
-				window.setTimeout(function() { $scope.facebook.searching = false; }, 1000);
-			});
-		}
-	});
 }
